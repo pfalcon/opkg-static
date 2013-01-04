b5af014022962f3f4fab145deb239685322a5330
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-01-04 19:49:37 +0200

    Add install (to device) script.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BASE=/data/local
+
+adb shell mkdir $BASE/bin
+adb shell mkdir $BASE/etc
+adb shell mkdir $BASE/etc/opkg
+adb shell mkdir $BASE/var/
+adb shell mkdir $BASE/var/lib
+adb shell mkdir $BASE/var/lib/opkg
+
+adb push resolv.conf $BASE/etc
+adb push opkg/install/bin/opkg-static $BASE/bin
+adb push options.conf $BASE/etc/opkg
+adb push arch.conf $BASE/etc/opkg
+adb push oe-android.conf $BASE/etc/opkg
+adb push oe $BASE
