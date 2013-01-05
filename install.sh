bd2ae42ce8e444c2b746cdac5ed81cde8cd14ccb
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-01-05 06:23:43 +0200

    Deploy /etc/passwd & /etc/group.

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@ adb shell mkdir $BASE/var/
 adb shell mkdir $BASE/var/lib
 adb shell mkdir $BASE/var/lib/opkg
 
+adb push passwd $BASE/etc
+adb push group $BASE/etc
 adb push resolv.conf $BASE/etc
+
 adb push opkg/install/bin/opkg-static $BASE/bin
 adb push options.conf $BASE/etc/opkg
 adb push arch.conf $BASE/etc/opkg
