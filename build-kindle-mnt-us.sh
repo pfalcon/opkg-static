f614d7e4abcc7c0499f38daa19bc417f0279a866
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-01-26 01:58:13 +0200

    Add build script for Kindle version.

---
+++ build-kindle-mnt-us.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Main prefix, opkg-static will be installed into $PREFIX/bin
+export PREFIX=/mnt/us
+# Where config files will be kept, "/opkg" will be appended automatically for opkg config files
+export SYSCONFDIR=$PREFIX/etc
+# Where package info files, etc. will be kept, "/opkg" will be appended
+# automatically
+export OPKG_LIB_DIR=$OPKG_PREFIX/var/lib
+
+. ./build.sh
