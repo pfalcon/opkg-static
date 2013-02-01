458bbfd16d4f33f28e8afdd0e2d73b7065e6ede2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-02-01 14:16:01 +0200

    Switch to keep config file in hierarchy.

--- install-android.sh
+++ install-android.sh
@@ -9,14 +9,14 @@ adb shell mkdir $BASE/var/
 adb shell mkdir $BASE/var/lib
 adb shell mkdir $BASE/var/lib/opkg
 
-adb push passwd $BASE/etc
-adb push group $BASE/etc
-adb push resolv.conf $BASE/etc
+adb push skel/etc/passwd $BASE/etc
+adb push skel/etc/group $BASE/etc
+adb push skel/etc/resolv.conf $BASE/etc
 
 adb push opkg/install/bin/opkg-static $BASE/bin
-adb push options.conf $BASE/etc/opkg
-adb push arch.conf $BASE/etc/opkg
-adb push oe-android.conf $BASE/etc/opkg
+adb push skel/etc/opkg/options.conf $BASE/etc/opkg
+adb push skel/etc/opkg/arch.conf $BASE/etc/opkg
+adb push skel/etc/opkg/oe-android.conf $BASE/etc/opkg
 adb push oe $BASE
 
 echo "Run:"
